495986e6804f8c87fd007553ff2d9bfb66a64b91
CHACCHAN <chacchan.info@gmail.com>
2024-05-19 23:26:47 +0900

    更新

--- web/src/components/pages/dashboardPage.vue
+++ web/src/components/pages/dashboardPage.vue
@@ -232,6 +232,14 @@ onMounted(() => {
                                                 </svg>
                                                 <div class="text-danger fw-bold" style="font-size: 12px;">停止</div>
                                             </div>
+                                            <div class="col-2 text-center">
+                                                <router-link class="text-light" :to="{ name: 'login' }">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-people-fill rotation" style="cursor: pointer;" viewBox="0 0 16 16">
+                                                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+                                                    </svg>
+                                                </router-link>
+                                                <div class="text-secondary fw-bold" style="font-size: 12px;">ユーザ切替</div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
